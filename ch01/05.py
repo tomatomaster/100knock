4bac44c7d87edc4f830751d9199ba05fec6b8e65
tomatomaster <iiitomatomasteriii@gmail.com>
2018-03-15 21:54:18 +0900

    Solve 05.py

--- ch01/05.py
+++ ch01/05.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-sentence = "test test1 test2 test3"
+sentence = raw_input()
 words = sentence.split()
 
 n = 2
-lists = []
+n_gram_senten = []
+n_gram_word = []
 
 for i in range(len(words)):
-    lists.append(words[i:i+n])
+    n_gram_senten.append(words[i:i+n])
 
-print lists
+print n_gram_senten
+
+for i in range(len(sentence)):
+    n_gram_word.append(sentence[i:i+n])
+
+print n_gram_word
