30fd2dad06961e07e23a0b6f1ebfefac8784cb17
小野 龍太郎 <ryuutaroh.ono@nts.ricoh.co.jp>
2018-03-19 12:57:16 +0900

    solve 09

---
+++ ch01/09.py
@@ -0,0 +1,15 @@
+import random
+
+data = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+data_list = data.split()
+for word in data_list:
+    if len(word) > 4:
+        target = word[1:-1]
+        target = ''.join(random.sample(target, len(target)))
+        word = word[0] + target + word[-1]
+        print word
+    else:
+        print word 
+
+
+        
